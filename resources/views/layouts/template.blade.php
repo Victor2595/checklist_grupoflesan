32eccb6da9e53fce624a4bd925575fbb2680dd81
Victor Flores <vflores@flesan.com.pe>
2019-09-12 16:36:59 -0500

    el finish

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -22,7 +22,7 @@
         <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('img/favicon/favicon-96x96.png') }}">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png') }}">
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" >
-        <!--<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">-->
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/ionicons.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/dataTables.bootstrap.min.css') }}">
